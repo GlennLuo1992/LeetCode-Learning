c3b5321f0465ca128e295e69f31115877a3e4abe
Luozunhua <luozunhua1993@qq.com>
2020-07-27 11:05:25 +0800

    2020-7-27

---
+++ src/practise2020/_15_getDecimalValue.java
@@ -0,0 +1,16 @@
+package practise2020;
+import neededClass.ListNode;
+
+public class _15_getDecimalValue {
+    public int getDecimalValue(ListNode head) {
+        return 0;
+    }
+    public static void main(String[] args) {
+        int res = 1;
+        int power =2;
+        for(int i = 0;i<power;i++){
+            res = res*2;
+        }
+        System.out.println(res);
+    }
+}
